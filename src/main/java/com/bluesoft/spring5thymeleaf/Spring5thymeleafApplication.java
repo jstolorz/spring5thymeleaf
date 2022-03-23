2d60689a5541810c0b9d9c13b80f5d2397cfbf2d
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-03-23 15:11:29 +0100

    Initial commit

---
+++ src/main/java/com/bluesoft/spring5thymeleaf/Spring5thymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.bluesoft.spring5thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5thymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5thymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluesoft/spring5thymeleaf/Spring5thymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesoft.spring5thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5thymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
